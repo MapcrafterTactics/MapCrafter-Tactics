f7671ee18c382f45cae51aa8c368cb6c4a3c85e2
Lvluch <148677265+Lvluch@users.noreply.github.com>
2025-12-01 18:42:18 +0200

    All buttons work

--- map-crafter-tactics/menu.gd
+++ map-crafter-tactics/menu.gd
@@ -1,7 +1,19 @@
 extends Control
 
 func _ready():
+	$ButtonsContainer/NewSceneButton.pressed.connect(_on_new_scene_pressed)
+	$ButtonsContainer/LoadSceneButton.pressed.connect(_on_load_scene_pressed)
+	$ButtonsContainer/SettingsButton.pressed.connect(_on_settings_pressed)
 	$ButtonsContainer/ExitButton.pressed.connect(_on_exit_pressed)
+	
+func _on_new_scene_pressed():
+	get_tree().change_scene_to_file("res://Editor.tscn")
+
+func _on_load_scene_pressed():
+	get_tree().change_scene_to_file("res://Saved scenes.tscn")
+
+func _on_settings_pressed():
+	get_tree().change_scene_to_file("res://Settings.tscn")
 
 func _on_exit_pressed():
 	get_tree().quit()
